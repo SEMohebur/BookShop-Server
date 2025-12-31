be92afd24848465637184dfc7e58feffd83a5491
MD. MOHIBUR <md.mohibur@Moheburs-MacBook-Air.local>
2025-12-31 23:23:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 3000;
+
+//middleeware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("bookShop");
+    const bookCollection = db.collection("books");
+
+    // all books get api
+    app.get("/books", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get single 1 data
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    });
+
+    //get recent 6 data
+    app.get("/recentBooks", async (req, res) => {
+      const cursor = bookCollection.find().sort({ createdAt: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // post api
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    });
+
+    // update api
+    app.patch("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateBook = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updateBook,
+      };
+      const result = await bookCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    //delete api
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
